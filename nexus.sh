7603e28d6cbb985c3afcadaf4b55609601a34ef3
Pesala Sreenivasulu <100116587+PesalaSreenivasulu@users.noreply.github.com>
2025-06-28 03:58:13 +0530

    Update nexus.sh

--- nexus.sh
+++ nexus.sh
@@ -1,35 +1,29 @@
-#create amazonlinux ec2 with t2.micro and 30 gb of ebs with port 8081 
-
+#PORT NUMBER: 8081
 sudo yum update -y
-sudo yum install wget -y
+sudo cd /opt/
 sudo yum install java-17-amazon-corretto-jmods -y
-sudo mkdir /app && cd /app
-sudo wget -O nexus.tar.gz https://download.sonatype.com/nexus/3/latest-unix.tar.gz
-sudo tar -xvf nexus.tar.gz
-sudo mv nexus-3* nexus
+sudo wget -O /opt/nexus.tar.gz https://download.sonatype.com/nexus/3/nexus-unix-x86-64-3.79.0-09.tar.gz
+ 
+sudo tar -xvf /opt/nexus.tar.gz -C /opt/
+sudo mv /opt/nexus-3* /opt/nexus
 sudo adduser nexus
-sudo chown -R nexus:nexus /app/nexus
-sudo chown -R nexus:nexus /app/sonatype-work
-sudo echo "run_as_user="nexus"" > /app/nexus/bin/nexus.rc
-sudo tee /etc/systemd/system/nexus.service > /dev/null << EOL
-[Unit]
-Description=nexus service
-After=network.target
-
-[Service]
-Type=forking
-LimitNOFILE=65536
-User=nexus
-Group=nexus
-ExecStart=/app/nexus/bin/nexus start
-ExecStop=/app/nexus/bin/nexus stop
-User=nexus
-Restart=on-abort
-
-[Install]
-WantedBy=multi-user.target
-EOL
-sudo chkconfig nexus on
-sudo systemctl start nexus
-sudo systemctl status nexus
-
+sudo chown -R nexus:nexus /opt/nexus
+sudo chown -R nexus:nexus /opt/sonatype-work
+#sudo echo "run_as_user="nexus"" > /opt/nexus/bin/nexus.rc
+#sudo tee /etc/systemd/system/nexus.service > /dev/null << EOL
+#[Unit]
+#Description=nexus service
+#After=network.target
+ 
+#[Service]
+#Type=forking
+#LimitNOFILE=65536
+#User=nexus
+ 
+#Restart=on-abort
+ 
+ 
+#[Install]
+#WantedBy=multi-user.target
+#EOL
+su - nexus -c "/opt/nexus/bin/nexus start"
